f4585beed15dfefc84ca45ad609390e04cf34326
Anthony Wong <wtz26@hotmail.com>
2019-04-02 23:58:20 +1100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="App">
+    <h1>Home Page</h1>
+
+    <div>
+      First Name:
+      <input v-model="firstName" placeholder="First Name"/>
+    </div>
+
+    <div>
+      Last Name:
+      <input v-model="lastName" placeholder="First Name"/>
+    </div>
+
+    <div>
+      ID:
+      <input v-model="id" placeholder="First Name"/>
+    </div>
+    
+    <button v-on:click="click" >GO</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    message: '',
+      firstName: '',
+      lastName: '',
+      id: '',
+      click: () =>{
+           window.location = '/user/'+this.firstName+"-"+this.lastName+"-"+this.id;
+      }
+    };
+  }
+};
+</script>
+
+<style>
+.App {
+     text-align: center;
+}
+</style>
+    
---
+++ pages/user/_user.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+    <h1>{{firstName+"  "+lastName}}</h1>
+    <p>Content: {{id}}</p>
+</div>
+</template>
+
+<script>
+export default {
+data(){
+        return{
+            firstName : null,
+            lastName : null,
+            id : null
+        }
+    },
+
+created(){
+    let user = this.$route.params.user;
+    user = user?user.split('-'):'';
+    this.firstName = user[0]?user[0].charAt(0).toUpperCase() + user[0].slice(1):'';
+    this.lastName = user[1]?user[1].charAt(0).toUpperCase() + user[1].slice(1):'';
+    this.id = user[2];
+},
+
+head() {    
+    return{
+        title: this.firstName,
+        meta:[
+           {name:this.firstName+this.lastName, content: this.id} 
+        ]
+    }
+}
+};
+</script>
